aff81b376b011f66ddddf31c3227596fa37d5403
= <=>
2023-06-17 16:10:59 +0100

    Backend

---
+++ src/test/java/tn/mdweb/aminoacido/AminoacidoApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.mdweb.aminoacido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AminoacidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
